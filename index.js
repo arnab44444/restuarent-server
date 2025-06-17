aca0a0f9af4ae84e3eaccc7dab9dce9de2d4aea8
arnab44444 <arnabbiswas661@gmail.com>
2025-06-17 19:30:48 +0600

    last commit

--- index.js
+++ index.js
@@ -71,21 +71,48 @@ async function run() {
     app.post("/place-order/:foodId", async (req, res) => {
       const id = req.params.foodId;
       const orderData = req.body;
+      const orderedQuantity = parseInt(orderData.quantity); // ensure it's a number
+
       console.log("Order received:", orderData);
 
       const result = await orderCollection.insertOne(orderData);
 
       if (result.acknowledged) {
-        // update quantity in coffees collection
-
+        // ✅ subtract ordered quantity from foods collection
         await foodsCollection.updateOne(
           { _id: new ObjectId(id) },
-          { $inc: { quantity: -1 } }
+          //{ $inc: { quantity: -orderedQuantity } }, // subtract user ordered quantity
+
+          {
+            $inc: {
+              quantity: -orderedQuantity, // Decrease quantity
+              purchasedCount: orderedQuantity, // Increase purchasedCount
+            },
+          }
         );
       }
 
       res.send(result);
     });
+
+    // app.post("/place-order/:foodId", async (req, res) => {
+    //   const id = req.params.foodId;
+    //   const orderData = req.body;
+    //   console.log("Order received:", orderData);
+
+    //   const result = await orderCollection.insertOne(orderData);
+
+    //   if (result.acknowledged) {
+    //     // update quantity in coffees collection
+
+    //     await foodsCollection.updateOne(
+    //       { _id: new ObjectId(id) },
+    //       { $inc: { quantity: -1 } }
+    //     );
+    //   }
+
+    //   res.send(result);
+    // });
 
     // my food page
 
@@ -130,17 +157,18 @@ async function run() {
     app.put("/foods/:id", async (req, res) => {
       const id = req.params.id;
 
-      const { _id, image, name, category, author, rating } = req.body;
+      const {
+        _id,
+        image,
+        name,
+        category,
+        price,
+        origin,
+        quantity,
+        description,
+      } = req.body;
 
       const query = { _id: new ObjectId(id) };
-
-      //const options  = {upsert : true};
-
-      // const updatedPlant = req.body;
-
-      // const updatedDoc = {
-      //   $set: updatedPlant
-      // }
 
       const updateData = {
         $set: {
@@ -164,28 +192,132 @@ async function run() {
 
     //
 
-     app.get("/myfood-post",
+    app.get(
+      "/myfood-post",
       // verifyToken,
-      
+
       async (req, res) => {
-      const email = req.query.email;
+        const email = req.query.email;
 
-      const query = { addedByEmail : email };
-      const jobs = await foodsCollection.find(query).toArray();
-      //console.log(email)
+        const query = { addedByEmail: email };
+        const jobs = await foodsCollection.find(query).toArray();
+        //console.log(email)
 
-      // should use aggregate to have optimum data fetching
+        // should use aggregate to have optimum data fetching
 
-      // for (const food of foods) {
-      //   const applicationQuery = { jobId: job._id.toString() };
-      //   const application_count = await applicationsCollection.countDocuments(
-      //     applicationQuery
-      //   );
-      //   job.application_count = application_count;
-      // }
-      res.send(jobs);
+        // for (const food of foods) {
+        //   const applicationQuery = { jobId: job._id.toString() };
+        //   const application_count = await applicationsCollection.countDocuments(
+        //     applicationQuery
+        //   );
+        //   job.application_count = application_count;
+        // }
+        res.send(jobs);
+      }
+    );
+
+    // delet food
+
+    app.delete("/cancel_Order/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      try {
+        // Find the order first
+        const order = await orderCollection.findOne(query);
+
+        if (!order) {
+          return res.status(404).send({ message: "Order not found" });
+        }
+
+        const foodId = order.foodId; // Make sure each order includes this when created
+        const orderQuantity = order.quantity || 1;
+
+        // Delete the order
+        const result = await orderCollection.deleteOne(query);
+
+        if (result.deletedCount > 0) {
+          // Increase food quantity in the food collection
+          // await foodsCollection.updateOne(
+          //   { _id: new ObjectId(foodId) },
+          //   {
+          //     $inc: { quantity: orderQuantity },
+          //     purchasedCount: -orderQuantity,
+          //   }
+          // );
+          await foodsCollection.updateOne(
+            { _id: new ObjectId(foodId) },
+            {
+              $inc: {
+                quantity: orderQuantity,
+                purchasedCount: -orderQuantity, // this line was the problem before
+              },
+            }
+          );
+        }
+
+        res.send(result);
+      } catch (error) {
+        console.error("Error canceling order:", error);
+        res.status(500).send({ message: "Server error" });
+      }
     });
 
+    // app.delete("/cancel_Order/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   //console.log('to be deleted', id)
+
+    //   const query = { _id: new ObjectId(id) };
+
+    //   const result = await orderCollection.deleteOne(query);
+
+    //   res.send(result);
+    // });
+
+    // nijer add kora food e order kora jabena
+
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+
+      const food = await foodsCollection.findOne({
+        _id: new ObjectId(order.foodId),
+      });
+
+      if (!food) {
+        return res.status(404).send({ message: "Food not found" });
+      }
+
+      // Check if buyer is the owner
+      if (food.addedByEmail === order.buyerEmail) {
+        return res
+          .status(400)
+          .send({ message: "You cannot order your own posted food." });
+      }
+
+      // Insert the order if valid
+      const result = await ordersCollection.insertOne(order);
+      res.send(result);
+    });
+
+    // top 6 purchase food
+
+    // new-plant section last 6ta data
+
+    // GET: Top 6 most purchased foods
+    app.get("/top-purchased-foods", async (req, res) => {
+      try {
+        const topFoods = await foodsCollection
+          .find()
+          .sort({ purchasedCount: -1 }) // descending order
+          .limit(6)
+          .toArray();
+
+        res.send(topFoods);
+      } catch (error) {
+        console.error("Error fetching top purchased foods:", error);
+        res.status(500).send({ error: "Internal Server Error" });
+      }
+    });
 
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
